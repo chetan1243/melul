7f89a9d06c00b543f96de7159bf61850254ceec6
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2018-12-16 08:26:04 +0530

    Add files via upload

--- commands/MODERATION/warn.js
+++ commands/MODERATION/warn.js
@@ -5,10 +5,10 @@ const ms = require("ms");
 module.exports.run = async (bot, message ,args) => {
 let warns = JSON.parse(fs.readFileSync("./data/warnings.json","utf8"));
 
-if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("you did't have permission to use this command! sorry.");
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("you did't have permission to use this command! sorry.");
 let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
 if(!wUser) return message.reply("could't find them yo!");
-if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("done")
+if(wUser.hasPermission("ADMINISTRATOR")) return message.reply("done")
 let reason = args.join(" ").slice(22);
 if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
@@ -33,11 +33,17 @@ let warnchannel = message.guild.channels.find(`name`, "incidents");
 if(!warnchannel) return("could't find channel");
 
 warnchannel.send(warnEmbed);
+if(warns[wUser.id].warns == 1){    
+    
+message.channel.send(`<@${wUser.id}> DON'T BREAK THE RULES OF GAMERS SUPPORTIVE COMMUNITY SERVER... THIS IS A WARNING TO YOU AFTER THIS YOU WILL BE MUTED AND IF U DID'T STOP THIS KIND OF WORK U WILL BE BANNED!`)
+    }
+
+
 
 if(warns[wUser.id].warns == 2){
 let muterole = message.guild.roles.find(`name`, "muted");
 if(!muterole) return message.reply("you should create that role dude!")
-
+message.channel.send(`<@${wUser.id}> DON'T BREAK THE RULES OF GAMERS SUPPORTIVE COMMUNITY SERVER... THIS IS A WARNING TO YOU AFTER THIS YOU WILL BE MUTED AND IF U DID'T STOP THIS KIND OF WORK U WILL BE BANNED!`)
 let mutetime = '1000s';
 await(wUser.addRole(muterole.id));
 message.channel.send(`<@${wUser.id}> has beed temp. muted!`)
