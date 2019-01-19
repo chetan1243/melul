d0c480ba8055e9c8eb534b760224fe6e2e10b957
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2019-01-19 06:39:44 +0530

    Add files via upload

--- commands/MODERATION/announcement.js
+++ commands/MODERATION/announcement.js
@@ -5,12 +5,17 @@ module.exports.run = async (bot, message ,args) => {
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("you did't have permission to use this command! sorry.");
     if(message.member.hasPermission("ADMINISTRATOR"));
     {
+        let text = args.join(" ")
+        ///let mention = message.guild.member(message.mentions.users.first()) || message.guild.users.get(args[0])
+      
+        let achannel = message.guild.channels.find(`name`, 'announcements');
         const embed = new Discord.RichEmbed()
         .setColor("#fc6400")
         .setTitle("Important Announcement:")
-        .setDescription(args.join(" "));
-        message.channel.send('@everyone')
-        message.channel.send(embed)
+        .setDescription(text);
+        achannel.send('@everyone')
+        achannel.send(embed)
+        
 
     }
 }
@@ -19,7 +24,7 @@ module.exports.command = {
 name: 'announcement',
 permission: "ADMINSTRATOR",
 description: "announcement",
-usage: `.announcement`,
+usage: `.announcement <message> channel`,
 category: "MODERATION",
 enabled: true
 };
