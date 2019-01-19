647ae2b16d39293cf4fb5b781923f455895af036
chetan1243 <45307042+chetan1243@users.noreply.github.com>
2019-01-19 18:18:02 +0530

    Add files via upload

--- commands/MODERATION/here announce.js
+++ commands/MODERATION/here announce.js
@@ -1,25 +1,7 @@
 const Discord = require('discord.js');
 const superagent = require('superagent');
 
-/*module.exports.run = async (bot, message ,args) => {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("you did't have permission to use this command! sorry.");
-    if(message.member.hasPermission("ADMINISTRATOR"));
-    {
-        let text = args.join(" ")
-    
-        let channel = message.mentions.channels.first() || message.guild.channels.get(args[0]);
-      
-        let achannel = message.guild.channels.find(`name`, channel);
-        const embed = new Discord.RichEmbed()
-        .setColor("#fc6400")
-        .setTitle("Important Announcement:")
-        .setDescription(text);
-        achannel.send('@everyone')
-        achannel.send(embed)
-        
 
-    }
-} */
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("you did't have permission to use this command! sorry.");
      let channel = message.mentions.channels.first() || message.guild.channels.get(args[0]);
@@ -28,17 +10,16 @@ module.exports.run = async (bot, message, args) => {
             .setColor("#fc6400")
             .setTitle("Important Announcement:")
             .setDescription(text);
-            channel.send('@everyone')
+            channel.send('@here')
             channel.send(embed)
     
     }
 
 module.exports.command = {
-name: 'eannounce',
+name: 'hannounce',
 permission: "ADMINSTRATOR",
 description: "announcement",
 usage: `.announcement <message> channel`,
 category: "MODERATION",
 enabled: true
 };
-
